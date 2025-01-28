535d127def0f0fe3f2fa3f187fd6325cd507333d
RohithaReddy33 <rohithareddyanneda@gmail.com>
2025-01-28 20:57:42 +0530

    Add files via upload

---
+++ opr.java
@@ -0,0 +1,23 @@
+public class opr {
+    public static void main (String[]args){
+        int a = 40;
+        int b = 50;
+        boolean c=true;
+        boolean d=false;
+//ARTHEMATIC OPERATIONS ON INTEGER DATA TYPE
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+//RELATIONAL OPERATION:
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a!=b);
+        System.out.println(a==b);
+//LOGICAL OPERATION:
+        System.out.println(c&&d);
+        System.out.println(c||d);
+    }
+}
